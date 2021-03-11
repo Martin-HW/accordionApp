6985ed427f983a7e527e8bd07d5339bb2c3f62ab
unknown <martin_suarez_mendez@outlook.com>
2021-03-11 11:30:18 -0600

    third commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save everything in here!!!
+          Edit <code>src/App.js</code> and save everything in here i'm a champ!!!
         </p>
         <a
           className="App-link"
